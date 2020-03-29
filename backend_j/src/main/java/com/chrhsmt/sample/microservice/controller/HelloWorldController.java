889d795de33c1303c987ffc6243c1a6a435f9485
Chihiro Hashimoto <chr@chrhsmt.com>
2020-03-30 00:28:02 +0900

    Update: java14 and add other sample controller

---
+++ backend_j/src/main/java/com/chrhsmt/sample/microservice/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.chrhsmt.sample.microservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class HelloWorldController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getHello() {
+        return "hello";
+    }
+}
